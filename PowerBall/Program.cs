caf9d9e3d1ad93288d554b35e8169609ca8ec366
zhangh4 <redrockets@gmail.com>
2015-02-02 20:39:02 -0600

    parsed winningNUmber

--- PowerBall/Program.cs
+++ PowerBall/Program.cs
@@ -11,12 +11,23 @@ namespace PowerBall
     {
         static void Main(string[] args)
         {
+            List<WinningNumber> winningNumbers = new List<WinningNumber>();
             using (var sr = new StreamReader("/users/p/documents/powerball-winning-numbers.txt"))
             {
+                sr.ReadLine(); // skip the column header line
                 string line;
                 while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(line);    
+                    Console.WriteLine(line);
+                    var parts = line.Split(new []{"  "}, StringSplitOptions.None);
+                    var winningNumber = new WinningNumber()
+                    {
+                        Date = DateTime.Parse(parts[0]),
+                        WhiteBalls = new HashSet<short>(parts.Skip(1).Take(5).Select(short.Parse)),
+                        RedBall = short.Parse(parts[6])
+                    };
+                    winningNumbers.Add(winningNumber);
+                    Console.WriteLine(winningNumber);
                 }
                 
             }
@@ -28,5 +39,13 @@ namespace PowerBall
         public DateTime Date { get; set; }
         public HashSet<short> WhiteBalls { get; set; }
         public short RedBall { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Date: {0}, WhiteBalls: {1}, RedBall: {2}", 
+                Date, 
+                "(" + WhiteBalls.Aggregate(string.Empty, (result, number) => result + " " + number) + ")", 
+                RedBall);
+        }
     }
 }
